3b3eb7a49d0d3fb1175bc4d38b264403d36d8921
Vicente Albonian <vicente.albonian@ticketek.com.au>
2024-06-19 17:11:33 +0800

    added optimization

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,10 @@ fn main() {
     App::new()
         .add_plugins((DefaultPlugins))
         .add_systems(Startup, (setup_camera))
-        .add_systems(Update, (handle_mouse, draw_pixel_grains))
+        .add_systems(Update, (handle_mouse, draw_pixel_grains, despawn_pixel_grains))
         .run();
 }
 
-const DIMENSION: i64 = 20;
 const PIXEL_SIZE: f32 = 10.0;
 
 /// Camera lerp factor.
@@ -104,6 +103,65 @@ fn draw_pixel_grains(
     
     let botton_right = (botton_right / 10.0).floor() * 10.0;
 
+    let pixel_grains_hash: bevy::utils::hashbrown::HashSet<(i64, i64)> = HashSet::from_iter(pixel_grains.iter().map(|(_, pg)|  (pg.x , pg.y)));
+    
+    let mut new_pixel_grains = vec![];
+    
+    for x in ((top_left.x.to_pixel_size() as i64)..(botton_right.x.to_pixel_size() as i64)).step_by(PIXEL_SIZE as usize) {
+        //println!("inside loop 1, {}", x);
+        for y in ((botton_right.y.to_pixel_size() as i64)..(top_left.y.to_pixel_size() as i64)).step_by(PIXEL_SIZE as usize)  {
+            //println!("inside loop 2, {}, {}", x, y);
+            if pixel_grains_hash.contains(&(x, y)) {
+                //println!("pixel_grains_hash.contains, {}, {}", x, y);
+                continue;
+            }
+
+            let color = Color::rgb(1.0,0.0,0.0);
+            let shape = Mesh2dHandle(meshes.add(Rectangle::new(PIXEL_SIZE, PIXEL_SIZE)));
+
+            //add to a vec so we can batch spawn them.
+            new_pixel_grains.push((PixelGrain::new(x as i64, y as i64), MaterialMesh2dBundle {
+                mesh: shape,
+                material: materials.add(color),
+                transform: Transform::from_xyz(
+                    // Distribute shapes from -X_EXTENT/2 to +X_EXTENT/2.
+                    x.to_pixel_size(), y.to_pixel_size(), 0.0,
+                ),
+                ..default()
+            }))
+
+        }
+    }
+
+    commands.spawn_batch(new_pixel_grains);
+
+}
+
+fn despawn_pixel_grains(camera_query: Query<(&Camera, &GlobalTransform)>,
+    mut commands: Commands,
+    pixel_grains: Query<(Entity, &PixelGrain)>){
+
+         //println!("Entering draw_pixel_grains");
+    let (camera, camera_transform) = camera_query.single();
+    
+    //println!("camera get");
+    let Some(view_port_size) = camera.logical_viewport_size() else {
+        return;
+    };
+    let width = view_port_size.x ;
+    let height = view_port_size.y;
+    let Some(top_left) = camera.viewport_to_world_2d(camera_transform, Vec2::new(0., 0.)) else {
+        return;
+    };
+    let top_left = top_left + Vec2::new(PIXEL_SIZE * -3.0, PIXEL_SIZE * 3.0);
+    let top_left = (top_left / 10.0).floor() * 10.0;
+
+    let Some(botton_right) = camera.viewport_to_world_2d(camera_transform, Vec2::new(width, height)) else {
+        return;
+    };
+    
+    let botton_right = (botton_right / 10.0).floor() * 10.0;
+
 
     let world_rect: Rect = Rect::from_corners(top_left, botton_right);
     for (entity, pixel_grain) in pixel_grains.iter() {
@@ -116,36 +174,6 @@ fn draw_pixel_grains(
         }
     }
 
-    let pixel_grains_hash: bevy::utils::hashbrown::HashSet<(i64, i64)> = HashSet::from_iter(pixel_grains.iter().map(|(_, pg)|  (pg.x , pg.y)));
-    // Calculate a world position based on the cursor's position.
-    //println!("pixel_grains_hash len: {}", pixel_grains_hash.len());
-    //println!("staring loop, {}, {}, {}, {}", top_left.x.to_pixel_size(), botton_right.x.to_pixel_size(), top_left.y.to_pixel_size(), botton_right.y.to_pixel_size());
-    for x in ((top_left.x.to_pixel_size() as i64)..(botton_right.x.to_pixel_size() as i64)).step_by(PIXEL_SIZE as usize) {
-        //println!("inside loop 1, {}", x);
-        for y in ((botton_right.y.to_pixel_size() as i64)..(top_left.y.to_pixel_size() as i64)).step_by(PIXEL_SIZE as usize)  {
-            //println!("inside loop 2, {}, {}", x, y);
-            if pixel_grains_hash.contains(&(x, y)) {
-                //println!("pixel_grains_hash.contains, {}, {}", x, y);
-                continue;
-            }
-
-            let color = Color::rgb(1.0,0.0,0.0);
-            let shape = Mesh2dHandle(meshes.add(Rectangle::new(PIXEL_SIZE, PIXEL_SIZE)));
-            //println!("spawn loc, {}, {}", x, y);
-            commands.spawn((PixelGrain::new(x as i64, y as i64), MaterialMesh2dBundle {
-                mesh: shape,
-                material: materials.add(color),
-                transform: Transform::from_xyz(
-                    // Distribute shapes from -X_EXTENT/2 to +X_EXTENT/2.
-                    x.to_pixel_size(), y.to_pixel_size(), 0.0,
-                ),
-                ..default()
-            }));
-
-        }
-
-        
-    }
 
 }
 
